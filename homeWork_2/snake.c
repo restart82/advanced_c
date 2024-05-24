f7a8c6c9ac0fd843bd6b9455249f3053b20e7beb
Petr <sh>
2024-05-24 15:34:00 +0300

    almost done
    нужно как то задать таймаут у функции getch()

--- homeWork_2/snake.c
+++ homeWork_2/snake.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-// #include <unistd.h>
 #include <windows.h>
 
-#define MAX_X 15
+#define MAX_X 30
 #define MAX_Y 15
 
-typedef struct tail_t
+#define SLEEP_TIME_MS 200
+
+typedef enum
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}command_t;
+
+typedef struct
 {
     int x;
     int y;
 }tail_t;
 
-typedef struct snake_t
+typedef struct
 {
     int x;
     int y;
-    struct tail_t * tail;
+    tail_t * tail;
     size_t tsize;
+    command_t currentCommand;
 }snake_t;
 
 // @**
@@ -34,7 +44,18 @@ snake_t initSnake(int x, int y, size_t tsize)
         snake.tail[i].x = x + i + 1;
         snake.tail[i].y = y;
     }
+    snake.currentCommand = LEFT;
     return snake;
+}
+
+void printLine()
+{
+    printf(".");
+    for (int i = 0; i < MAX_X; i++)
+    {
+        printf("_");
+    }
+    printf(".\n");
 }
 
 void printSnake(snake_t snake)
@@ -54,12 +75,7 @@ void printSnake(snake_t snake)
         matrix[snake.tail[i].x][snake.tail[i].y] = '*';
     }
 
-    printf(".");
-    for (int i = 0; i < MAX_X; i++)
-    {
-        printf("_");
-    }
-    printf(".\n");
+    printLine();
     for (int j = 0; j < MAX_Y; j++)
     {
         printf("|");
@@ -69,47 +85,106 @@ void printSnake(snake_t snake)
         }
         printf("|\n");
     }
-    printf(".");
-    for (int i = 0; i < MAX_X; i++)
-    {
-        printf("_");
-    }
-    printf(".\n");
+    printLine();
 
 }
 
-snake_t moveLeft(snake_t snake)
+void moveSnake(snake_t* snake)
 {
-    for (int i = snake.tsize - 1; i > 0; i--)
+    for (int i = snake->tsize - 1; i > 0; i--)
     {
-        snake.tail[i] = snake.tail[i - 1];
+        snake->tail[i] = snake->tail[i - 1];
     }
-    snake.tail[0].x = snake.x;
-    snake.tail[0].y = snake.y;
-    snake.x = snake.x - 1;
+    snake->tail[0].x = snake->x;
+    snake->tail[0].y = snake->y;
 
-    if (snake.x < 0)
+    switch (snake->currentCommand)
     {
-        snake.x = MAX_X - 1;
+    case UP:
+        snake->y = snake->y - 1;
+        if (snake->y < 0)
+        {
+            snake->y = MAX_Y - 1;
+        }
+        break;
+    case DOWN:
+        snake->y = snake->y + 1;
+        if (snake->y >= MAX_Y)
+        {
+            snake->y = 0;
+        }
+        break;
+    case LEFT:
+        snake->x = snake->x - 1;
+        if (snake->x < 0)
+        {
+            snake->x = MAX_X - 1;
+        }
+        break;
+    case RIGHT:
+        snake->x = snake->x + 1;
+        if (snake->x >= MAX_X)
+        {
+            snake->x = 0;
+        }
+        break;
     }
-
-    return snake;
 }
 
+void setCommand(char command, snake_t* snake)
+{
+    switch (command)
+    {
+    case 'w':
+    case 'W':
+        if (snake->currentCommand != DOWN)
+        {
+            snake->currentCommand = UP;
+        }
+        break;
+    case 'd':
+    case 'D':
+        if (snake->currentCommand != LEFT)
+        {
+            snake->currentCommand = RIGHT;
+        }
+        break;
+    case 's':
+    case 'S':
+        if (snake->currentCommand != UP)
+        {
+            snake->currentCommand = DOWN;
+        }
+        break;
+    case 'a':
+    case 'A':
+        if (snake->currentCommand != RIGHT)
+        {
+            snake->currentCommand = LEFT;
+        }
+        break;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    snake_t snake = initSnake(10, 5, 2);
+    snake_t snake = initSnake(10, 5, 5);
     printSnake(snake);
+    char command;
 
     while (1)
     {
-        snake = moveLeft(snake);
-        Sleep(100);
+        command = getch();
+        if (command == 'q' || command == 'Q')
+        {
+            return 0;
+        }
+        setCommand(command, &snake);
+        moveSnake(&snake);
+        Sleep(SLEEP_TIME_MS);
         system("cls");
         printSnake(snake);
     }
 
-    
     return 0;
 }
